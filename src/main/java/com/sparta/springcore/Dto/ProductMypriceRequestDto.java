60f4be5aaeb0dac838b8b858a9a07aa338f2c978
unknown <ghksgml0807@naver.com>
2025-12-02 17:32:03 +0900

    환경설정

---
+++ src/main/java/com/sparta/springcore/Dto/ProductMypriceRequestDto.java
@@ -0,0 +1,12 @@
+package com.sparta.springcore.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ProductMypriceRequestDto {
+    private int myprice;
+}
